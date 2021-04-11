db343c49581f51ff926279af610eb00436bce0d4
jinshengli <1515026890@qq.com>
2021-04-12 00:45:47 +0800

    springcloud starter

---
+++ eureka-server/src/test/java/com/ljs/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,14 @@
+package com.ljs.eurekaserver;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
